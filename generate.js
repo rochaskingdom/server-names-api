ee494b8666a9f7bd2fbbaa110586f0d1ceac6eb6
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-02-20 20:10:23 -0300

    Gerando e testando dados aleatorios

--- generate.js
+++ generate.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const faker = require('faker');
 const Person = require('./person.js');
-
-mongoose.connect('mongodb://localhost:27017/namesdb', { useNewUrlParser: true});
-
+ 
+mongoose.connect('mongodb://localhost:27017/namesdb', { useNewUrlParser: true, useUnifiedTopology: true }  );
+ 
 async function createRandomPeople() {
     const N = 1000;
-    for (let i = 0; i < N; i++) {
+    for(let i=0;i<N;i++) {
         let p = new Person({
             firstname: faker.name.firstName(),
-            lastname: faker.name.lastname(),
+            lastname: faker.name.lastName(), 
             email: faker.internet.email(),
             city: faker.address.city(),
-            country: faker.address.country()
+            country:  faker.address.country()
         });
         try {
             await p.save();
@@ -22,8 +22,8 @@ async function createRandomPeople() {
         }
     }
 }
-
-createRandomPeople().then(() => {
-    mongoose.disconnect();
-    console.log('OK');
-});
+ 
+createRandomPeople().then(()=>{
+    mongoose.disconnect();    
+    console.log("OK");
+})
