12b28227646fb6280b5b14f12e8a5183113d7c87
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-02-20 20:30:03 -0300

    Criando Serviço Web

---
+++ app.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Person = require('./person.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/namesdb', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+    Person.find({}).lean()
+    .exec((err, data) => {
+        if (err) {
+            return res.status(500).json({
+                error: err,
+                message: 'Internal error'
+            });
+        }
+        return res.status(200).json(data);
+    });
+});
+
+app.use(function(req, res, next) {
+    res.status(404).send('Route does not exist.');
+});
+
+app.listen(9000);
